ad516cd84b5b530f2c56dfd2b8b9328ebaffcff3
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2020-02-17 09:41:58 +0100

    Added Day 03 exercise 01

---
+++ Day03/exercise01.js
@@ -0,0 +1,59 @@
+let firstName = "Oluwafikayo";
+let lastName = "Ajayi";
+let country = "NIGERIA";
+let age = 23;
+let isMarried = true;
+let year = new Date().getFullYear();
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+
+console.log('10' === 10);
+
+console.log(parseInt('9.8') == 10);
+
+let isTrue = 45 > 41;
+let isFalse = 45 > 100;
+
+
+
+console.log(4 > 3)
+console.log(4 >= 3)
+console.log(4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4)
+console.log(4 === 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 != '4')
+console.log(4 == '4')
+console.log(4 === '4')
+console.log('jargon'.length == 'python'.length)
+
+
+console.log(4 > 3 && 10 < 12)
+console.log(4 > 3 && 10 > 12)
+console.log(4 > 3 || 10 < 12)
+console.log(4 > 3 || 10 > 12)
+console.log(!(4 > 3))
+console.log(!(4 < 3))
+console.log(!(false))
+console.log(!(4 > 3 && 10 < 12))
+console.log(!(4 > 3 && 10 > 12))
+console.log(!(4 === '4'))
+console.log('dragon'.includes('on')  && 'python'.includes('on'));
+
+
+
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth());
+console.log(new Date().getDate());
+console.log(new Date().getDay());
+console.log(new Date().getHours());
+console.log(new Date().getMinutes());
+console.log(Date.now());
+
