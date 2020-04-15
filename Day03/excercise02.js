c2e844180c2c71c28731caac1e0cba54a66dd579
ajafik <oluwafikayoajayi@gmail.com>
2020-04-15 11:16:10 +0100

    Added excercise 2 of day 3

---
+++ Day03/excercise02.js
@@ -0,0 +1,26 @@
+// Area of Triangle
+let base = + prompt("Enter base: ");
+let height  = + prompt("Enter Height: ");
+let area  = 0.5 * base * height;
+
+
+// Perimeter of Triangle 
+let a  = + prompt("Enter a ");
+let b  = + prompt("Enter b ");
+let c  = + prompt("Enter c ");
+let perimeter = a + b + c;
+
+
+// Area of Rectangle 
+let length  = + prompt("Enter Length ");
+let width = + prompt("Enter Width ");
+let rec_perimeter  =  2 * (length + width);
+
+// Area and Perimeter of Circle 
+let radius  = + prompt("Enter Radius ");
+let area_circle  = 3.14 * radius* radius;
+let circumference_circle = 2 * 4.14 * radius;
+
+
+// 
+
